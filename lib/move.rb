9d7c05dd29b2ff232d7fb1ab99a785ac616afe4b
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-07 21:49:28 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
 user_input.to_i - 1
 end
 
-def move(board,index,value = "X")
-  board[index] = value
+def move(board, index, current_player = "X")
+  board[index] = current_player 
 end
